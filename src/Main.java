7b7911485d4aa8b1eeeb1fb67be34b23a1a5a3d3
J-Leader <126532557+J-Leader@users.noreply.github.com>
2025-08-15 10:55:14 +1000

    Part 4 done, to be seperated into class files

--- src/Main.java
+++ src/Main.java
@@ -10,27 +10,73 @@ public class Main extends JFrame {
       window.run();
     }
 
-    class Canvas extends JPanel {
-      public Canvas() {
+    class Canvas extends JPanel { // Canvas Class
+      public Canvas() { // Constructor
         setPreferredSize(new Dimension(720, 720));
       }
 
-      @Override
-    public void paint(Graphics g) {
-      int gridDimensions = 35;
+     
 
-	  g.setColor(java.awt.Color.BLACK);
-	  g.drawRect(10, 10, 700, 700);
-        for(int x = 10; x<710; x+=gridDimensions){
-          for(int y =10; y<710; y+=gridDimensions){
-            g.drawRect(x, y, gridDimensions, gridDimensions);
+    @Override
+    public void paint(Graphics g) {
+	  Cell initialCell = new Cell(35, 35, 10, 10);
+    Grid Grid = new Grid(20, 20, initialCell);
+        Grid.paint(g);
+
+      }
+    }
+
+ class Cell{ // Cell Class for rectangles
+        public Cell(int width, int height, int xPos, int yPos)//Constructor
+        {
+          cellWidth = width;
+          cellHeight = height;
+          cellXPos = xPos;
+          cellYPos = yPos;
+
+        }
+        int cellWidth;
+        int cellHeight;
+        int cellXPos;
+        int cellYPos;
+
+        public void paint(Graphics g){
+          g.setColor(java.awt.Color.BLACK);
+          g.drawRect(cellXPos, cellYPos, cellWidth, cellHeight);
+        }
+
+      }
+
+class Grid{ //Grid Class
+        public Grid(int rows, int columns, Cell cell) //Constructor, takes amount of rows and columns and what kind of cells it should be using.
+        {
+          gridNumRows = rows;
+          gridNumColumns = columns;
+          gridCellTemplate = cell;
+        }
+        int gridNumRows;
+        int gridNumColumns;
+        Cell gridCellTemplate;
+        Cell gridCell;
+
+        int offset = 10;
+
+        public void paint(Graphics g)
+        {
+          g.setColor(java.awt.Color.BLACK);
+          for(int x =0;x < gridNumRows;x++){
+          for(int y =0; y<gridNumColumns; y++){
+            gridCell = new Cell(gridCellTemplate.cellWidth, gridCellTemplate.cellHeight,  offset + x*gridCellTemplate.cellWidth,  offset + y*gridCellTemplate.cellHeight);
+            gridCell.paint(g);
           }
 
 
         }
+        }
+
 
       }
-    }
+
 
     private Main() {
       this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);

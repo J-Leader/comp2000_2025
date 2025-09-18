83193cfe100053081f7591af8f5ebea908658d1a
J-Leader <126532557+J-Leader@users.noreply.github.com>
2025-09-18 10:04:16 +1000

    Removing animalBehaviour implementation
    Removing implementation of deleted animalBehaviour script

--- src/Actor.java
+++ src/Actor.java
@@ -4,7 +4,7 @@ import java.awt.Polygon;
 import java.util.ArrayList;
 import java.util.List;
 
-public abstract class Actor implements animalBehaviour {
+public abstract class Actor{
   Color color;
   Cell loc;
   List<Polygon> display;

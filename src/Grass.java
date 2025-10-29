e0ae5c594507d849133856f58a99fe954584b48b
J-Leader <126532557+J-Leader@users.noreply.github.com>
2025-10-29 17:54:13 +1100

    Stashed cell types pushed
    Accidentally stashed the cell types, see previous commit for detail

---
+++ src/Grass.java
@@ -0,0 +1,15 @@
+
+import java.awt.Color;
+
+public class Grass extends Cell{
+
+    //grass is basic tile and goes everywhere that isn't lake or shore
+    
+public Grass (char inCol, int inRow, int xPos, int yPos) 
+{
+super(inCol, inRow, xPos, yPos); //Super Cell constructor to ensure that col and row values are set and rectangle super is also used.
+cellColor = Color.GREEN;
+cellAltitude = 0;
+}
+
+}
---
+++ src/Lake.java
@@ -0,0 +1,12 @@
+import java.awt.Color;
+
+public class Lake extends Cell{
+    
+public Lake(char inCol, int inRow, int xPos, int yPos) // re-use given cell constructor
+{
+super(inCol, inRow, xPos, yPos);
+cellColor = Color.BLUE;
+cellAltitude = -15;
+}
+
+}

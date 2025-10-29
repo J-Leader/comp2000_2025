b62e32b8219428f3d2f4d810750b196d31dccfc9
J-Leader <126532557+J-Leader@users.noreply.github.com>
2025-10-29 17:48:27 +1100

    Week 11 class changes

--- src/Client.java
+++ src/Client.java
@@ -1,10 +1,10 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 public class Client {
@@ -12,7 +12,7 @@ public class Client {
     public static void main(String[] args) throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://13.238.167.130/times"))
+                .uri(URI.create("http://13.238.167.130/weather"))
                 .header("Accept", "text/event-stream")
                 .build();
 
@@ -20,10 +20,25 @@ public class Client {
                 .thenApply(HttpResponse::body)
                 .thenAccept(inputStream -> {
                     try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
-                        String line;
+                        /*String line;
                         while ((line = reader.readLine()) != null) {
                                 System.out.println("Received: " + line);
-                        }
+                        }*/
+
+                        reader.lines()
+                                .map(line -> line.split(" "))
+                                .forEach(pieces ->{
+                                    System.out.println("Receieved new weather event at time:" + pieces[0]);
+                                    //pieces 1 is weather condition
+                                    //pieces 2 is x coord
+                                    //pieces 3 is y coord
+                                    //pieces 4 is float value
+
+
+
+                                })
+                                ;
+
                     } catch (IOException e) {
                         System.err.println("Error reading Server Side Event (SSE) stream: " + e.getMessage());
                     }
